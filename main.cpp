94182ab63e708291b29f60ef3870642b5cfe23e0
nnzgab <52886845+nnzgab@users.noreply.github.com>
2024-07-07 16:13:46 -0300

    Update main.cpp
    se definen 3 leds y 3 botones

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
+#define LED_1 PB_12
+#define LED_1 PB_13
+#define LED_1 PB_14
+
+
+#define BOTON1 PA_8
+#define BOTON2 PA_9
+#define BOTON3 PA_10
 
 int main()
 {
