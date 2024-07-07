dabc69a06c83ba8e70b1d6bc3d14e2ea774140cf
nnzgab <52886845+nnzgab@users.noreply.github.com>
2024-07-07 18:22:24 -0300

    Update main.cpp
    se reorganiza el codigo en funciones
    y se acomoda codigo y  comentarios

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//=====[Libraries]=============================================================
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 
@@ -11,44 +13,108 @@
 #define BOTON2 PA_9
 #define BOTON3 PA_10
 
+//=====[Declaration and initialization of public global objects]===============
+DigitalIn tecla_1(BOTON1);
+DigitalIn tecla_2(BOTON2);
+DigitalIn tecla_3(BOTON3);
+
+DigitalOut ledArm(LED_1);
+DigitalOut ledReady(LED_2);
+DigitalOut ledSystem(LED_3);
+
+//UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+
+//=====[Declaration and initialization of public global variables]=============
+
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void inputsInit();
+void outputsInit();
+
+void botonCopiaLed();
+
+//void uartTask();
+//void availableCommands();
+
+
+//=====[Main function, the program entry point after power on or reset]========
+
 int main()
 {
-    DigitalIn tecla_1(BOTON1);
-    DigitalIn tecla_2(BOTON2);
-    DigitalIn tecla_3(BOTON3);
+    inputsInit();
+    outputsInit();
 
-    DigitalOut ledArm(LED_1);
-    DigitalOut ledReady(LED_2);
-    DigitalOut ledSystem(LED_3);
+    while (true) {
+        botonCopiaLed();
+        //uartTask();
+    }
+}
 
+//=====[Implementations of public functions]===================================
+
+void inputsInit()
+{
     tecla_1.mode(PullDown);
     tecla_2.mode(PullDown);
     tecla_3.mode(PullDown);
+}
 
+void outputsInit()
+{
     ledArm = OFF;
     ledReady = OFF;
-    ledSystem = OFF;
+    ledSystem = OFF;;
+}
 
-    while (true) {
-        if ( tecla_1 == ON ) {
-            ledArm = ON;
-        }
-        else {
-            ledArm = OFF;
-        }
-        
-        if ( tecla_2 == ON ) {
-            ledReady = ON;
-        }
-        else {
-            ledReady = OFF;
-        }
+void botonCopiaLed(){
+    
+    if ( tecla_1 == ON ) {
+    ledArm = ON;
+    }
+    else {
+        ledArm = OFF;
+    }
+    
+    if ( tecla_2 == ON ) {
+        ledReady = ON;
+    }
+    else {
+        ledReady = OFF;
+    }
+    
+    if ( tecla_3 == ON ) {
+        ledSystem = ON;
+    }
+    else {
+        ledSystem = OFF;
+    }
+}
 
-        if ( tecla_3 == ON ) {
-            ledSystem = ON;
-        }
-        else {
-            ledSystem = OFF;
+
+/*
+
+void uartTask()
+{
+    char receivedChar = '\0';
+    if( uartUsb.readable() ) {
+        uartUsb.read( &receivedChar, 1 );
+        if ( receivedChar == '1') {
+            if ( alarmState ) {
+                uartUsb.write( "The alarm is activated\r\n", 24);
+            } else {
+                uartUsb.write( "The alarm is not activated\r\n", 28);
+            }
+        } else {
+            availableCommands();
         }
     }
 }
+
+void availableCommands()
+{
+    uartUsb.write( "Available commands:\r\n", 21 );
+    uartUsb.write( "Press '1' to get the alarm state\r\n\r\n", 36 );
+}
+
+*/
